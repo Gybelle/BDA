fc90bf118d9b2e685bbc12328492cf992051d31d
Michelle Gybels <michelle.gybels@student.uhasselt.be>
2016-10-05 15:20:48 +0200

    First version of aPriori

--- Project01-FrequentItemsets/Assignment01.py
+++ Project01-FrequentItemsets/Assignment01.py
@@ -1,22 +1,64 @@
 # Assignment 1: Collaboration Groups
 # Authors: Michelle Gybels & Anaïs Ools
 
-index = 0
-buckets = {}
-threshold = 10
+import itertools
 
-with open("../Input/authorsperpublication.txt", "r") as file:
-    for line in file:
-        for author in line.replace("[","").replace("]","").strip().split(","):
-            key = frozenset({author})
-            if key in buckets:
-                buckets[key] += 1
-            else:
-                buckets[key] = 1
+threshold = 25
+
+def aPriori():
+    list = applyThreshold(firstStep())
+    for i in range(2, 4):
+        list = applyThreshold(step(i, list))
+
+def firstStep():
+    candidates = {}
+    with open("../Input/authorsperpublication.txt", "r") as file:
+        for line in file:
+            for author in line.replace("[", "").replace("]", "").strip().split(","):
+                key = frozenset({author})
+                if key in candidates:
+                    candidates[key] += 1
+                else:
+                    candidates[key] = 1
+    file.close()
+    return candidates
+
+def applyThreshold(candidates):
+    list = []
+    for authors in candidates:
+        if(candidates[authors] >= threshold):
+            list.append(authors)
+    return list
+
+def step(k, list):
+    print("Step %d:" % (k))
+    candidates = createNewCandidates(list, k)
+    print("\t Candidates created.")
+    with open("../Input/authorsperpublication.txt", "r") as file:
+        for line in file:
+            for authors in candidates:  #change this
+                allPresent = True
+                for author in authors:
+                    if allPresent and author not in line:
+                        allPresent = False
+                if allPresent:
+                    candidates[authors] += 1
+    file.close()
+    for authors in candidates:
+        print("%s -> %s" % (authors, candidates[authors]))
+    return candidates
 
 
-for author in buckets:
-    if(buckets[author] >= threshold):
-        print("%s -> %s" % (author, buckets[author]))
+def createNewCandidates(list, k):
+    authorCollection = []
+    for authors in list:
+        authorCollection.extend(authors)
+    keys = itertools.combinations(authorCollection, k)
+    print("\t Candidates intialised.")
+    candidates = {}
+    for key in keys:
+        candidates[key] = 0
+    return candidates
 
-file.close()
+
+aPriori()
