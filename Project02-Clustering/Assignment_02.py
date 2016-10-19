ef30512b7f8f9577cef766a607a9669050e04e2d
Anaïs Ools <ools.anais@gmail.com>
2016-10-19 16:40:45 +0200

    Stemming experiment

--- Project02-Clustering/Assignment_02.py
+++ Project02-Clustering/Assignment_02.py
@@ -1,1 +1,23 @@
-print("hellow")
+# Assignment 2: Clustering
+# Authors: Michelle Gybels & Anaïs Ools
+
+from stemming.porter2 import stem
+import re
+
+file = open("../Input/titles.txt", "r")
+# results = open("../Output/results.txt", "a")
+
+index = 0
+for line in file:
+    if index < 20:
+        line = line.strip().replace("[", "").replace("]", "")
+        for word in line.split(" "):
+            word = re.sub(r'\W+', '', word).lower()
+            if word:
+                print(stem(word), end=" ")
+        print()
+    index += 1
+
+
+file.close()
+# results.close()
