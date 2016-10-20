e737142e84d4d604b5a5466e70933f6f34a8b564
Anaïs Ools <ools.anais@gmail.com>
2016-10-20 14:29:38 +0200

    Clustering works for entire dataset

--- Project02-Clustering/Assignment_02.py
+++ Project02-Clustering/Assignment_02.py
@@ -3,8 +3,15 @@
 
 from stemming.porter2 import stem
 import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
-bigDataset = False
+bigDataset = True
+k = 2
+
 if not bigDataset:
     file = open("../Input/titles.txt", "r", encoding="utf8")
     # results = open("../Output/titlesResult.txt", "a", encoding="utf8")
@@ -24,6 +31,46 @@ def stemLines(lines):
             result.append(stemmedLine.strip())
     return result
 
-print(stemLines(file))
+def executeKMeans(k, titleVectors):
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(titleVectors.toarray())
+    return kmeans
+
+def printClusters(topicRange, clusterList, k):
+    print("Top topics per cluster:")
+    order_centroids = clusterList.cluster_centers_.argsort()[:, ::-1]
+    topics = vectorizer.get_feature_names()
+    for i in range(k):
+        print("Cluster %d:" % (i+1))
+        for j in order_centroids[i, :topicRange]:
+            print(' %s' % topics[j])
+        print()
+
+def plotClusters(titleVectors, clusterList):
+    # Plot: data
+    pca = PCA(n_components=2).fit(titleVectors.toarray())
+    data = pca.transform(titleVectors.toarray())
+    plt.scatter(data[:, 0], data[:, 1], c=clusterList.labels_)
+
+    # Plot: centers
+    centers = pca.transform(clusterList.cluster_centers_)
+    plt.hold(True)
+    plt.scatter(centers[:, 0], centers[:, 1], marker='x', s=200, linewidths=2, c='r')
+    plt.show()
+
+
+
+stemmedTitles = stemLines(file)
+
+# vectorize
+vectorizer = TfidfVectorizer(stop_words='english')
+titleVectors = vectorizer.fit_transform(stemmedTitles)
+
+kmeans = executeKMeans(k, titleVectors)
+
+# printClusters(topicRange, clusterList, k):
+printClusters(10, kmeans, k)
+plotClusters(titleVectors, kmeans)
+
 file.close()
 # results.close()
