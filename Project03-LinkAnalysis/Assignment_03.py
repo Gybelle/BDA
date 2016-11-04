070b7cd15fddd3da045859a4c5a8485e1af61a68
Anaïs Ools <ools.anais@gmail.com>
2016-11-04 13:15:05 +0100

    IET WORKS ofzo groetjes

--- Project03-LinkAnalysis/Assignment_03.py
+++ Project03-LinkAnalysis/Assignment_03.py
@@ -7,9 +7,11 @@ import matplotlib.pyplot as plt
 import csv
 
 bigDataset = True
-labelThreshold = 0.07
+labelThreshold = 0.08
+dataElement = 2  # 0 = publ. count, 1 = PageRank, 2 = authority
 authorMap = {}
 authorGraph = nx.Graph()
+highlightNames = {"Frank Neven", "Jan Van den Bussche", "Marc Gyssens", "Stijn Vansummeren", "Bas Ketsman", "Tom J. Ameloot"}
 
 if not bigDataset:
     file = open("../Input/pods.txt", "r", encoding="utf8")
@@ -21,24 +23,45 @@ else:
 def printAuthorGraph():
     d = {}
     lab = {}
-    weights = []
+    labCol = {}
+    calculatedThreshold = calculateThreshold()
+    threshold = calculatedThreshold[0]
+    factor = calculatedThreshold[1]
     for node in authorGraph.nodes():
-        weights.append(float(authorMap[node][1]))
+        size = authorMap[node][dataElement] * factor  # set size of node
+        d[node] = size
+        if size > threshold:
+            if node in highlightNames:
+                labCol[node] = node  # set label
+            else:
+                lab[node] = node
+        else:
+            if node in highlightNames:
+                labCol[node] = ""
+            else:
+                lab[node] = ""
+    print(labCol)
+
+    pos = nx.random_layout(authorGraph)
+    nx.draw(authorGraph, pos=pos, nodelist=d.keys(), node_size=[v * 100 for v in d.values()], labels=lab, with_labels=True, node_color='c', edge_color='#9F9F9F', font_weight='bold')
+    nx.draw_networkx_labels(authorGraph, pos=pos, labels=labCol, font_weight='bold', font_color='#E12707')
+    plt.show()
+
+def calculateThreshold():
+    weights = []
+    if dataElement == 0:
+        factor = 1
+    elif dataElement == 1:
+        factor = 1000
+    elif dataElement == 2:
+        factor = 1000
+    for node in authorGraph.nodes():
+        weights.append(float(authorMap[node][dataElement]))
     weights.sort()
     labThreshold = int(round(len(weights)*(1.0-labelThreshold), 0))
     while labThreshold > 0 and labThreshold < len(weights)-1 and weights[labThreshold-1] == weights[labThreshold]:
         labThreshold += 1
-    labThreshold = weights[labThreshold] * 1000
-
-    for node in authorGraph.nodes():
-        size = authorMap[node][1] * 1000  # set size of node
-        d[node] = size
-        if size > labThreshold:
-            lab[node] = node  # set label
-        else:
-            lab[node] = ""
-    nx.draw_random(authorGraph, nodelist=d.keys(), node_size=[v * 100 for v in d.values()], labels=lab, with_labels=True, node_color='c', edge_color='#7F7F7F', font_weight='bold')
-    plt.show()
+    return weights[labThreshold] * factor, factor
 
 def createNetwork():
     """
@@ -84,8 +107,6 @@ def printResults():
         csvWriter.writerow(['Author', 'Publication count', 'PageRank', 'Authority score'])
         for author in authorMap:
             csvWriter.writerow([author, authorMap[author][0], authorMap[author][1], authorMap[author][2]])
-
-
 
 createNetwork()
 perfomLinkAnalysis()
